740e9e938032f694671111d8cd4bc8266320a367
div112 <63148072+div112@users.noreply.github.com>
2021-02-17 20:10:35 -0800

    Add files via upload

---
+++ American Options.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[78]:
+
+
+import numpy as np
+from scipy.stats import norm
+
+
+# In[79]:
+
+
+def call_pricer(n, S, K, r,q, v, t):  
+##for american Call option
+
+    dt = t/n 
+
+    u = np.exp(v*np.sqrt(dt))
+
+    d = 1/u
+
+    p = (np.exp((r-q)*dt)-d) / (u-d) 
+    #For the binom price tree
+    val = np.zeros((n+1,n+1))
+    val[0,0] = S
+    for i in range(1,n+1):
+        val[i,0] = val[i-1,0]*u
+        for j in range(1,i+1):
+            val[i,j] = val[i-1,j-1]*d           
+    # final node   
+    optionvalue = np.zeros((n+1,n+1))
+    for j in range(n+1):
+            optionvalue[n,j] = max(0, val[n,j]-K)
+    #backward calc
+    for i in range(n-1,-1,-1):
+        for j in range(i+1):
+            optionvalue[i,j] = max(0, val[i,j]-K, np.exp(-r*dt)*(p*optionvalue[i+1,j]+(1-p)*optionvalue[i+1,j+1]))
+
+    return optionvalue[0,0]
+
+
+# In[80]:
+
+
+print("Value of American Call option is:", call_pricer(100, 100, 100, 0.02, 0.08, 0.35, 1))
+
+
+# In[ ]:
+
+
+
+
+
+# In[81]:
+
+
+def greeks(n, S, K, r,q, v, t):  
+##for american Call option
+
+    dt = t/n 
+
+    u = np.exp(v*np.sqrt(dt))
+
+    d = 1/u
+
+    p = (np.exp((r-q)*dt)-d) / (u-d) 
+    #For the binom price tree --> s
+    val = np.zeros((n+1,n+1))
+    val[0,0] = S
+    for i in range(1,n+1):
+        val[i,0] = val[i-1,0]*u
+        for j in range(1,i+1):
+            val[i,j] = val[i-1,j-1]*d  
+    # final node  --> f 
+    optionvalue = np.zeros((n+1,n+1))
+    for j in range(n+1):
+            optionvalue[n,j] = max(0, val[n,j]-K)
+    #backward calc
+    for i in range(n-1,-1,-1):
+        for j in range(i+1):
+            optionvalue[i,j] = max(0, val[i,j]-K, np.exp(-r*dt)*(p*optionvalue[i+1,j]+(1-p)*optionvalue[i+1,j+1]))
+
+    Delta= (optionvalue[1,0]- optionvalue[1,1])/(val[1,0]- val[1,1])
+    d2= (optionvalue[2,0] - optionvalue[2,1])/(val[2,0]- val[2,1])
+    d1= (optionvalue[2,1]- optionvalue[2,2])/(val[2,1]- val[2,2])
+    Gamma= (d2-d1)/ (val[2,1]- val[2,2])
+    Theta= (optionvalue[2,1]- optionvalue[0,0])/(0.02)
+
+    return Delta, Gamma, Theta
+
+
+# In[82]:
+
+
+greeks_1= greeks(100, 100, 100, 0.02, 0.08, 0.35, 1)
+
+
+# In[83]:
+
+
+Vega = (call_pricer(100, 100, 100, 0.02, 0.08, 0.36, 1)- call_pricer(100, 100, 100, 0.02, 0.08, 0.35, 1))/ 0.01
+Vega
+
+
+# In[84]:
+
+
+Rho= (call_pricer(100, 100, 100, 0.03, 0.08, 0.35, 1)- call_pricer(100, 100, 100, 0.02, 0.08, 0.35, 1))/ 0.01
+Rho
+
+
+# In[85]:
+
+
+print("Price of the American Call option is: ", call_pricer(100, 100, 100, 0.02, 0.08, 0.35, 1))
+print("Delta is equal to: ", greeks_1[0])
+print("Gamma is equal to: ", greeks_1[1])
+print("Theta is equal to: ", greeks_1[2])
+print("Vega is equal to: ", Vega )
+print("Rho is equal to: ", Rho)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ MCMC Pricer.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[78]:
+
+
+##importing libs
+import math
+import numpy
+from numpy import *
+from time import time
+
+
+# In[79]:
+
+
+random.seed(15000)
+t0 = time()
+# the necessary parameters for the BSM eq
+# current stock price
+S0 = 100.
+#strike price
+K = 100.
+#time to maturity in years
+T = 1.0; 
+#rf rate ; volatility
+r = 0.02; sigma = 0.35
+#continuous dividend yield
+q= 0.08
+M = 100; dt = T / M; I = 2000
+
+
+
+# In[80]:
+
+
+##Price obtained using BSM formula
+st_bsm= 10.56
+
+
+# In[81]:
+
+
+import numpy as np 
+import scipy as sp 
+def option_payoff(S, K, option_type):     
+    payoff = 0.0     
+    if option_type == "call":         
+        payoff = max(S - K, 0)     
+    elif option_type == "put":         
+        payoff = max(K - S, 0)     
+    return payoff 
+
+
+# In[82]:
+
+
+S0 = 100.0 
+K = 100.0 
+T = 1.0 
+r = 0.02 
+q = 0.08 
+sigma = 0.35 
+option_type = "call" 
+n_steps = 100 
+#number of steps 
+sp.random.seed(7321) 
+#make result reproducable 
+n_simulation = 2000 
+#number of simulation 
+dt = T/n_steps 
+sqrt_dt = np.sqrt(dt) 
+payoff = np.zeros((n_simulation), dtype = float) 
+step = range(0, int(n_steps), 1) 
+
+for i in range(0, n_simulation):     
+    ST = S0     
+    for j in step[:-1]:         
+        epsilon = sp.random.normal()         
+        ST *= np.exp((r - q - 0.5*sigma*sigma)*dt + sigma*epsilon*sqrt_dt)     
+        payoff[i] = option_payoff(ST, K, option_type) 
+        
+        
+option_price1 = np.mean(payoff)*np.exp(-r*T) 
+print(option_type + ' price = ', option_price1)
+print("Error is:", st_bsm-option_price1)
+
+
+# In[83]:
+
+
+## Part a
+
+#Simulating I paths with M time steps
+
+S= S0 * exp(cumsum((r - q - 0.5 * sigma ** 2) * dt + sigma * math.sqrt(dt) * random.standard_normal((M + 1, I)), axis=0))
+#MC estimator
+C0 = math.exp(-r * T) * sum(maximum(S[-1] - K, 0)) / I
+
+
+print('The European Option Value is: ', C0) 
+print('Error is:', C0- st_bsm)
+
+
+##first 20 paths 
+import matplotlib.pyplot as plt
+plt.plot(S[:, :20])
+plt.grid(True)
+plt.xlabel('Steps')
+plt.ylabel('Index level')
+plt.show()
+
+
+# In[85]:
+
+
+##Part b-- Antithetic Sampling
+
+
+# In[86]:
+
+
+#S1= S0 * exp(cumsum((r - q - 0.5 * sigma ** 2) * dt + sigma * math.sqrt(dt) * 
+            #(-random.standard_normal((M + 1, I))), axis=0))
+#MC estimator
+#C1 = math.exp(-r * T) * sum(maximum(S1[-1] - K, 0)) / I
+for i in range(0, n_simulation):     
+    ST = S0     
+    for j in step[:-1]:         
+        epsilon = -sp.random.normal()         
+        ST *= np.exp((r - q - 0.5*sigma*sigma)*dt + sigma*epsilon*sqrt_dt)     
+        payoff[i] = option_payoff(ST, K, option_type) 
+        
+        
+option_price2 = np.mean(payoff)*np.exp(-r*T) 
+print(option_type + ' price = ', option_price2)
+print("Error is:", st_bsm-option_price2)
+
+#print('The European Option Value is: ', C1) 
+#print('Error is:', C1- st_bsm)
+
+
+# In[87]:
+
+
+#plt.plot(S1[:, :20])
+#plt.grid(True)
+#plt.xlabel('Steps')
+#plt.ylabel('Index level')
+#plt.show()
+
+
+# In[88]:
+
+
+#control variate
+beta= 0.5
+expected_ST= S0* np.exp((r-q)*T)
+for i in range(0, n_simulation):     
+    ST = S0     
+    for j in step[:-1]:         
+        epsilon = sp.random.normal()         
+        ST *= np.exp((r - q - 0.5*sigma*sigma)*dt + sigma*epsilon*sqrt_dt)     
+        payoff[i] = option_payoff(ST, K, option_type) -  beta *(ST- expected_ST)
+        
+        
+option_price3 = np.mean(payoff)*np.exp(-r*T) 
+print(option_type + ' price = ', option_price3)
+print("Error is: ", st_bsm - option_price3)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
